021f6d53ab9338e173b02fb8c282507e357eef17
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-04-20 19:27:05 +0700

    [VuTM] TODO: ironsource

--- Assets/Script/Advertising/AdsControllers.cs
+++ Assets/Script/Advertising/AdsControllers.cs
@@ -10,15 +10,28 @@ public class AdsControllers: MonoBehaviour
     public string appKey;
     private bool available;
 
+    private void Awake()
+    {
+        IronSource.Agent.init(appKey);
+    }
+
     private void Start()
     {
         IronSource.Agent.shouldTrackNetworkState(true);
-        IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
-        IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdsClosedEvent;
+        //Add AdInfo Rewarded Video Events
+        IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
+        IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
+        IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
+        IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
+        IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
+        IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
+        IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
+
     }
 
     public void Rewarded()
     {
+        Debug.Log("xem");
         IronSource.Agent.showRewardedVideo();
     }
 
@@ -31,4 +44,37 @@ public class AdsControllers: MonoBehaviour
     {
         bool rewardedVideoAvailability = available;
     }
+    
+/************* RewardedVideo AdInfo Delegates *************/
+// Indicates that there’s an available ad.
+// The adInfo object includes information about the ad that was loaded successfully
+// This replaces the RewardedVideoAvailabilityChangedEvent(true) event
+    void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo) {
+    }
+// Indicates that no ads are available to be displayed
+// This replaces the RewardedVideoAvailabilityChangedEvent(false) event
+    void RewardedVideoOnAdUnavailable() {
+    }
+// The Rewarded Video ad view has opened. Your activity will loose focus.
+    void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo){
+    }
+// The Rewarded Video ad view is about to be closed. Your activity will regain its focus.
+    void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo){
+        IronSource.Agent.init(appKey,IronSourceAdUnits.REWARDED_VIDEO);
+        IronSource.Agent.shouldTrackNetworkState(true);
+    }
+// The user completed to watch the video, and should be rewarded.
+// The placement parameter will include the reward data.
+// When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback.
+    void RewardedVideoOnAdRewardedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo){
+    }
+// The rewarded video ad was failed to show.
+    void RewardedVideoOnAdShowFailedEvent(IronSourceError error, IronSourceAdInfo adInfo){
+    }
+// Invoked when the video ad was clicked.
+// This callback is not supported by all networks, and we recommend using it only if
+// it’s supported by all networks you included in your build.
+    void RewardedVideoOnAdClickedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo){
+    }
+
 }
